934d7723619e4deb5b0dad02de7f0aa83a98b62f
Meet Makwana <meetmakwana203@gmail.com>
2024-11-28 16:39:59 +0530

    chore: fixed eslint error

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -72,31 +72,29 @@ const Home: React.FC<IHomeProps> = (props) => {
       headerData: {
         actions: [],
         title: (
-          <>
-            <div className="brain-wave-table-header-wrapper">
-              <div className="heading-for-table-text">Recent</div>
-              <div style={{ display: "flex", gap: "16px", marginBottom: "16px" }}>
-                <CustomBigButton
-                  label="New Document"
-                  icon={<Icon icon="NewTab" version="v2" size="small" />}
-                  onClick={() => handleNewDocumentButtonClick()}
-                  isActive={true} /* Set to true if active */
-                />
-                <CustomBigButton
-                  label="Templates"
-                  icon={<Icon icon="Layout" version="v2" size="small" />}
-                  onClick={() => handleButtonClick("Templates")}
-                  isActive={true} /* Set to true if active */
-                />
-                <CustomBigButton
-                  label="Import"
-                  icon={<Icon icon="Download" version="v2" size="small" />}
-                  onClick={() => handleButtonClick("Import")}
-                  isActive={false} /* Set to true if active */
-                />
-              </div>
+          <div className="brain-wave-table-header-wrapper">
+            <div className="heading-for-table-text">Recent</div>
+            <div style={{ display: "flex", gap: "16px", marginBottom: "16px" }}>
+              <CustomBigButton
+                label="New Document"
+                icon={<Icon icon="NewTab" version="v2" size="small" />}
+                onClick={() => handleNewDocumentButtonClick()}
+                isActive={true} /* Set to true if active */
+              />
+              <CustomBigButton
+                label="Templates"
+                icon={<Icon icon="Layout" version="v2" size="small" />}
+                onClick={() => handleButtonClick("Templates")}
+                isActive={true} /* Set to true if active */
+              />
+              <CustomBigButton
+                label="Import"
+                icon={<Icon icon="Download" version="v2" size="small" />}
+                onClick={() => handleButtonClick("Import")}
+                isActive={false} /* Set to true if active */
+              />
             </div>
-          </>
+          </div>
         ),
       },
       onclick: () => {
